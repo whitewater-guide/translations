80054dbaa6f9571140bfbf20068fe90712119367
doomsower <K.Kuznetcov@gmail.com>
2019-06-08 12:51:07 +0300

    Add some files

---
+++ packages/mobile/resources/en/auth.ts
@@ -0,0 +1,9 @@
+const auth = {
+  logoutDialogTitle: 'Sign Out',
+  logoutDialogMessage: 'Are you sure want to sign out?',
+  logoutErrorTitle: 'Error',
+  logoutErrorMessage:
+    'Could not sign out. Please check your internet connection.',
+};
+
+export default auth;
---
+++ packages/mobile/resources/en/offline.ts
@@ -0,0 +1,18 @@
+const offline = {
+  dialog: {
+    title: 'Download for offline use',
+    subtitle: 'Selected region: {{region}}',
+    categories: {
+      data: 'Detailed sections info *',
+      media: 'Photos',
+      maps: 'Maps (coming soon)',
+    },
+    cacheWarning: '* This data will be lost when you logout or change language',
+    loadingSummary: 'Loading metadata',
+    inBackground: 'Continue in background',
+    summaryError: 'Error while loading metadata',
+    download: 'Download',
+  },
+};
+
+export default offline;
